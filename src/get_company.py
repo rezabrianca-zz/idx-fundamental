8a936c577cf2563faef48f5b0bc26e2c0b2cc27e
rbrianca <reza.brianca@gmail.com>
2020-10-22 21:41:52 +0700

    Add headless information
    Include properties to enable headless scrape and upload data to Google Sheets

--- src/get_company.py
+++ src/get_company.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import ElementClickInterceptedException
 import pandas as pd
+import gspread
 import time
 import os
 
@@ -15,9 +16,16 @@ print('Begin job at {0}'.format(today))
 opts = Options()
 opts.add_argument('log-level=3') # suppress warning
 opts.add_argument('--no-sandbox')
+opts.add_argument('--headless')
+opts.add_argument('--disable-gpu')
+opts.add_argument('--disable-features=NetworkService')
+opts.add_argument('--disable-dev-shm-usage')
+opts.add_argument('--disable-features=VizDisplayCompositor')
+opts.add_argument('--window-size=1920x1080')
 
 # remove comment and change the path to your chromedriver directory
 # browser = Chrome('/Users/username/Downloads/chromedriver', options=opts)
+gc = gspread.oauth()
 
 browser.implicitly_wait(1)
 
@@ -29,14 +37,14 @@ try:
     select = Select(browser.find_element_by_name('companyTable_length'))
     number_per_page = int(select.options[3].text)
     select.select_by_value('100')
-    time.sleep(0.5)
+    time.sleep(1)
 
     company_df = pd.DataFrame()
 
     # get page number to fetch
     page_element = browser.find_elements_by_class_name('paginate_button ')
     page_element_length = len(page_element) + 1 # to capture last page
-    time.sleep(0.5)
+    time.sleep(1)
 
     # retrieve data in the each page
     for i in range(1, page_element_length):
@@ -64,6 +72,14 @@ try:
 
     # store in csv
     company_df.to_csv('./data/company_code_{0}.csv'.format(today), index=False)
+    
+#   update with Google Sheets Key
+#   sh = gc.open_by_key("GOOGLE_SHEETS_KEY")
+    worksheet = sh.worksheet("Company List")
+    print('Updating Company List ...')
+    worksheet.update([company_df.columns.values.tolist()] + company_df.values.tolist())
+    print('Upload Success')
+    
     print('There are {0} public companies at {1}'.format(company_df.shape[0], today))
 
 finally:
